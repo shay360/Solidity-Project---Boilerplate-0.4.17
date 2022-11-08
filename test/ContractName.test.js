748e3aff8b8678483179327ed5edaaa4eb6cec7a
shyzby <>
2022-11-08 06:58:29 +0000

    adding a get accounts for tests

--- test/ContractName.test.js
+++ test/ContractName.test.js
@@ -2,3 +2,17 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // Constructor function
 const web3 = new Web3(ganache.provider());
+const mocha = require('mocha');
+
+let accounts;
+beforeEach(async () => {
+	// Get a list of test accounts
+ accounts = await web3.eth.getAccounts();
+	// Use account for contract deployment
+});
+
+describe('Contract Test', () => {
+	it('Deploy Contract', () => {
+		console.log(accounts);
+	});
+});
